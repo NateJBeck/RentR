932e64c141addbc3c59417fdc1fa9a1c96e25d56
Chen Huang <alan.tolearn@gmail.com>
2014-11-10 15:33:50 -0500

    Show a specific listing

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
 
   resources :users, only: [:new, :create, :show] do
-    resources :listings, only: [:index, :new, :create]
+    resources :listings, only: [:new, :create]
   end
+
+  resources :listings, only: [:index, :show]
 
   root "listings#index"
 end

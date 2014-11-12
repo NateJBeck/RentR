04428d84ebaa1817311d0fe7f5b58da93c32c2a3
Nathan Beck <nathan.beck10@gmail.com>
2014-11-12 11:00:07 -0500

    Display region show page

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,4 +3,13 @@ class LocationsController < ApplicationController
     @locations = Location.all
   end
 
+  def show
+    @location = find_location_from_url
+  end
+
+  private
+
+  def find_location_from_url
+    Location.find(params[:id])
+  end
 end

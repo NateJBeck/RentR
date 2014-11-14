3cf4480b31b7a893568a84697209c8f92b742494
Chen Huang <alan.tolearn@gmail.com>
2014-11-14 10:17:27 -0500

    Set bounds for listings

--- app/assets/javascripts/geocoder-simple.js
+++ app/assets/javascripts/geocoder-simple.js
@@ -11,6 +11,7 @@ function createMap(){
   };
 
   window.map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+  window.mapBound = new google.maps.LatLngBounds();
 }
 
 function addLocations() {
@@ -24,16 +25,21 @@ function geocodeAndAddMarker(address){
   geocoder.geocode( { 'address': address }, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
       var result_location = results[0].geometry.location;
-
-      map.setCenter(result_location)
       var marker = new google.maps.Marker({
         map: window.map,
         position: result_location
       });
+
+      setBounds(result_location)
     } else {
       alert("Geocode was not successful for the following reason: " + status);
     }
   });
+}
+
+function setBounds(latlng) {
+  window.mapBound.extend(latlng);
+  window.map.fitBounds(window.mapBound)
 }
 
 //google.maps.event.addDomListener(window, 'load', initialize);
